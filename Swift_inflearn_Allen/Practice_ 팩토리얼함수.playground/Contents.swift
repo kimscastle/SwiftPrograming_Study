5ee56a830b18c11999ed24676032d0c24400b324
Youth <99013115+kimscastle@users.noreply.github.com>
2022-06-25 17:28:34 +0900

    Feat: Practice_팩토리얼함수

---
+++ Swift_inflearn_Allen/Practice_ 팩토리얼함수.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+func factorial(_ input: Int) -> Int {
+    var result: Int = 1
+    for i in 1...input {
+        result *= i
+    }
+    return result
+}
+
+print(factorial(5))
+
+//재귀함수
+func factorialF(num: Int) -> Int {
+    //재귀함수에서는 함수를 멈추게할 논리를 넣어줘야한다
+    //그렇지않으면 stack over flow가 발생한다
+    if num <= 1 {
+        return 1
+    }
+    return num * factorialF(num: num - 1)
+}
+
+factorialF(num: 5)
