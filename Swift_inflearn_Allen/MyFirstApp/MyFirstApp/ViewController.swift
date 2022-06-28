cf18775e91754484f7215a40d9cdf4f9ab04b6b2
Youth <99013115+kimscastle@users.noreply.github.com>
2022-06-29 01:20:30 +0900

    Feat: 앱 만들기 / 코드에 대한 자세한 설명 (68강)

--- Swift_inflearn_Allen/MyFirstApp/MyFirstApp/ViewController.swift
+++ Swift_inflearn_Allen/MyFirstApp/MyFirstApp/ViewController.swift
@@ -10,19 +10,25 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    
+    // Attribute: 속성
+    // Interface Builder Outlet
+    // 배출구(코드에서 화면으로 나감)
     @IBOutlet weak var mainLabel: UILabel!
     
     
-    
+    // 앱의 화면에 들어오면 처음 실행시키는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mainLabel.backgroundColor = UIColor.yellow
     }
 
-    
+    // Interface Builder Action
+    // 삽입구(화면에서 코드 안으로 들어옴)
     @IBAction func buttonPressed(_ sender: UIButton) {
         
         mainLabel.text = "안녕하세요"
+        mainLabel.backgroundColor = UIColor.clear
 //        mainLabel.backgroundColor = UIColor.green
 //        mainLabel.textColor = UIColor.white
 //        mainLabel.textAlignment = NSTextAlignment.left
