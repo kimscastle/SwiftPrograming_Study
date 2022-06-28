5293d90cc135faaeaadd7b4549dde1d86fe78d10
Youth <99013115+kimscastle@users.noreply.github.com>
2022-06-29 01:05:41 +0900

    Feat: 앱 만들기 / 코드 설정하기 (67강)

--- Swift_inflearn_Allen/MyFirstApp/MyFirstApp/ViewController.swift
+++ Swift_inflearn_Allen/MyFirstApp/MyFirstApp/ViewController.swift
@@ -4,16 +4,34 @@
 //
 //  Created by uiskim on 2022/06/26.
 //
-
+// 화면 하나당 화면을 관리한는 하나의 코드가있다
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        mainLabel.text = "안녕하세요"
+//        mainLabel.backgroundColor = UIColor.green
+//        mainLabel.textColor = UIColor.white
+//        mainLabel.textAlignment = NSTextAlignment.left
+        
+        
+    }
+    
+    
+    
 
 }
 
