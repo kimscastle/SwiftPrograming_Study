46cdf79536652dc671f2c8c508fd6fcb51c66ddb
Youth <99013115+kimscastle@users.noreply.github.com>
2022-06-30 01:02:20 +0900

    Feat: 앱 만들기 / 코드 설정하기 - 2 (75강)

---
+++ Swift_inflearn_Allen/RPSGame/RPSGame/Rps.swift
@@ -0,0 +1,15 @@
+//
+//  Rps.swift
+//  RPSGame
+//
+//  Created by uiskim on 2022/06/30.
+//
+
+import Foundation
+
+// 원시값(raw value)
+enum Rps: Int {
+    case rock
+    case paper
+    case scissors
+}
