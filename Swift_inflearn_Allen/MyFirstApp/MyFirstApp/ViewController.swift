64acecd4a24e60405a788c1a709a58a1795b2119
kimscastle <uisong26559287@gmail.com>
2022-08-16 10:34:52 +0900

    :sparkles: 버튼누르면버튼색깔바뀌는 기능추가

--- Swift_inflearn_Allen/MyFirstApp/MyFirstApp/ViewController.swift
+++ Swift_inflearn_Allen/MyFirstApp/MyFirstApp/ViewController.swift
@@ -9,10 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var mainLabel: UILabel!
-
-
+    @IBOutlet weak var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -21,12 +20,10 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
         mainLabel.text = "안녕하세요"
         mainLabel.textColor = UIColor.white
-        //mainLabel.backgroundColor = UIColor.systemIndigo
         mainLabel.backgroundColor = #colorLiteral(red: 0.8155849576, green: 0.637394309, blue: 1, alpha: 1)
         mainLabel.textAlignment = NSTextAlignment.left
+        
+        myButton.backgroundColor = UIColor.red
     }
-    
-
-
 }
 
