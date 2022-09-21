91cb3d04ea490a97500da7eea700bf3fb7f46e28
kimscastle <uisong26559287@gmail.com>
2022-09-21 23:30:36 +0900

    :recycle: slider를 selector로 구현

--- Swift_inflearn_Allen/TimeProgressApp/TimeProgressApp/ViewController.swift
+++ Swift_inflearn_Allen/TimeProgressApp/TimeProgressApp/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     // reset버튼을 누르면 가운데로 와야함
     @IBOutlet weak var slider: UISlider!
         
+    // Timer는 class기 때문에 힙영역에 저장된다
+    // 참조로인한 누수를 방지하기 위해 weak으로 선언
     weak var timer: Timer?
     var number: Int = 0
     
@@ -39,6 +41,7 @@ class ViewController: UIViewController {
         // 정수형으로 바꾸면 소수점은 버림처리
         // 실제 value는 0~1인데 그걸 60을 곱해서 0~60처럼 보이게 세팅
         let seconds = Int(slider.value * 60)
+        // == let seconds = Int(sender.value * 60)
         mainLabel.text = "\(seconds) 초"
         number = seconds
     }
@@ -46,28 +49,49 @@ class ViewController: UIViewController {
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
         // 1초씩 지나갈때마다 무언가를 실행
+        // startbutton을 두번누르면 초기화가 안되면 타이머가 두개 세팅됨
          timer?.invalidate()
-        // 객체 내에서 클로저를 사용할때는 변수에 self를 붙여줘야함
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [self] _ in
-            // 반복을 하고 싶은 코드
-            if number > 0 {
-                number -= 1
-                // 숫자를 계산해서 넣어줘야함
-                // 슬라이더의 위치도 바뀌어야함
-                slider.value = Float(number) / Float(60)
-                // 레이블표시도 다시 해줘야 함
-                mainLabel.text = "\(number) 초"
-                
-            } else {
-                number = 0
-                mainLabel.text = "초를 선택하세요"
-                timer?.invalidate()
-                
-                // 소리를 나게 해야함
-                AudioServicesPlayAlertSound(SystemSoundID(1322))
-            }
-        }
         
+        // 객체 내에서 클로저를 사용할때는 변수에 self를 붙여줘야함
+//        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [self] _ in
+//            // 반복을 하고 싶은 코드
+//            if number > 0 {
+//                number -= 1
+//                // 숫자를 계산해서 넣어줘야함
+//                // 슬라이더의 위치도 바뀌어야함
+//                slider.value = Float(number) / Float(60)
+//                // 레이블표시도 다시 해줘야 함
+//                mainLabel.text = "\(number) 초"
+//
+//            } else {
+//                number = 0
+//                mainLabel.text = "초를 선택하세요"
+//                timer?.invalidate()
+//
+//                // 소리를 나게 해야함
+//                AudioServicesPlayAlertSound(SystemSoundID(1322))
+//            }
+//        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(doSomethingAfter1Second), userInfo: nil, repeats: true)
+    }
+    
+    @objc func doSomethingAfter1Second() {
+        if number > 0 {
+            number -= 1
+            // 숫자를 계산해서 넣어줘야함
+            // 슬라이더의 위치도 바뀌어야함
+            slider.value = Float(number) / Float(60)
+            // 레이블표시도 다시 해줘야 함
+            mainLabel.text = "\(number) 초"
+        } else {
+            number = 0
+            mainLabel.text = "초를 선택하세요"
+            timer?.invalidate()
+
+            // 소리를 나게 해야함
+            AudioServicesPlayAlertSound(SystemSoundID(1322))
+        }
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
