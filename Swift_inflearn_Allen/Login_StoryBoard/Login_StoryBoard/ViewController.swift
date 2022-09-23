c22b7867fedd3ec2b9dcc976a1bf0f18adce50d9
kimscastle <uisong26559287@gmail.com>
2022-09-23 23:53:08 +0900

    :lipstick: 텍스트필드 ui설정

--- Swift_inflearn_Allen/Login_StoryBoard/Login_StoryBoard/ViewController.swift
+++ Swift_inflearn_Allen/Login_StoryBoard/Login_StoryBoard/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textField: UITextField!
+        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        render()
+    }
+    
+    func render() {
+        view.backgroundColor = .gray
+        textField.keyboardType = UIKeyboardType.emailAddress
+        textField.placeholder = "이메일 입력"
+        // textField의 borderStyle은 .roundRect가 default
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .done
     }
 
 
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        
+    }
 }
 
