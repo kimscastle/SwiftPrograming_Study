7a3f1276e76bc64d4fc1e8d7288197bfb32bf47f
kimscastle <uisong26559287@gmail.com>
2022-09-25 21:26:57 +0900

    :sparkles: 빈곳누르면 키보드 내려가능 기능 구현

--- Swift_inflearn_Allen/Login_StoryBoard/Login_StoryBoard/ViewController.swift
+++ Swift_inflearn_Allen/Login_StoryBoard/Login_StoryBoard/ViewController.swift
@@ -17,7 +17,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         textField.delegate = self
-        self.hideKeyboardWhenTappedAround()
         render()
     }
     
@@ -30,11 +29,23 @@ class ViewController: UIViewController {
         textField.clearButtonMode = .always
         // 키보드의 가장 오른쪽 아래버튼을 어떤버튼으로 보이게할지
         textField.returnKeyType = .done
+        
+        //MARK : - responder응답자에 대한 이해가 필요함
+        // 자동으로 focus가 textField에 맞춰지게됨
+        // 화면에서 일단 먼저 반응할 녀석
+        textField.becomeFirstResponder()
     }
 
-
+    // 키보드가 내려가게 하고 싶음
     @IBAction func doneButtonTapped(_ sender: UIButton) {
-        
+        // becomeFirstRsponder과 반대의 메서드
+        textField.resignFirstResponder()
+    }
+    
+    // touchBegan은 화면의 탭을 감지하는 메서드임
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        // textField.resignFirstResponder()
     }
 }
 
@@ -58,8 +69,6 @@ extension ViewController: UITextFieldDelegate {
     // 한글자한글자에 조건을 달 수 있다
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let maxLength: Int = 10
-//        print(string)
-//        return true
         guard let text = textField.text else { return true }
         let newLength = text.count + string.count - range.length
         return newLength <= maxLength
@@ -68,39 +77,26 @@ extension ViewController: UITextFieldDelegate {
     // 텍스트필드의 엔터키를 누르면 다음동작을 허락할것인지 말것인지
     // 예를들어 엔터키를 누르면 다음 텍스트필드로 커서를 옮길수있게
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        if textField.text == "" {
-//            textField.placeholder = "Type Something!"
-//            return false
-//        } else {
-//            return true
-//        }
-        textField.resignFirstResponder()
-        return true
+        if textField.text == "" {
+            textField.placeholder = "Type Something!"
+            // 엔터키를 눌러도 다음동작을 허락하지 않는다
+            return false
+        } else {
+            // 엔터키를 누르면 다음동작을 허락한다
+            textField.resignFirstResponder()
+            guard let text = textField.text else {return false}
+            print(text)
+            return true
+        }
     }
     
     // EndEditing은 텍스트필드에서 focus가 꺼졌을때를 의미함
-    // 텍스트필드의 입력이 끝날때 호출(끝낼수있게 만들지 return버튼을 사용할수있게만들지)
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         return true
     }
     
-    // 텍스트필드의 입력이 끝날때 호출(return을 누르면 어떤 action을 할지)
+    // 텍스트필드의 입력이 끝날때 호출(focus가 꺼지면 어떤 action을 할지)
     func textFieldDidEndEditing(_ textField: UITextField) {
         print("유저가 텍스트필드의 입력을 끝냈다.")
     }
 }
-
-// 키보드 숨기기
-extension UIViewController {
-    func hideKeyboardWhenTappedAround() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        // true이면 제스처를 인식하면 View로 touchesCancelled 메시지를 보내 이전에 전달된 터치 이벤트를 취소하고, 이전에 전달된 이후의 이벤트들을 View로 전달하지 않음
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
-    
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-}
-
