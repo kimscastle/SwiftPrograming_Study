71a3638925328540c1714aac72bf48c3ee35969b
kimscastle <uisong26559287@gmail.com>
2022-09-29 18:40:04 +0900

    :tada: SnapKit연습 프로젝트 생성

---
+++ TrialAndError/SnapKit_Practice/SnapKit_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapKit_Practice
+//
+//  Created by uiskim on 2022/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
