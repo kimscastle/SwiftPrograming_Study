39b89145282314c324de00ccc6ab5e9e9367b40a
kimscastle <uisong26559287@gmail.com>
2022-09-30 23:15:18 +0900

    :lipstick: mainView ui구현

---
+++ Swift_inflearn_Allen/BMI_calculator/BMI_calculator/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  BMI_calculator
+//
+//  Created by uiskim on 2022/09/30.
+//
+
+import UIKit
+import SnapKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
--- Swift_inflearn_Allen/BMI_calculator/BMI_calculator/ViewController.swift
+++ Swift_inflearn_Allen/BMI_calculator/BMI_calculator/ViewController.swift
@@ -12,6 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
 
+    //MARK : - 코드로 화면이동(다음화면이 코드로 작성되어있을때 사용)
+    @IBAction func codeNextButtonTapped(_ sender: UIButton) {
+    }
+    
+    //MARK : - 코드로 스토리보드 객체를 생성해서 화면이동
+    @IBAction func storyboardWithCodeButtonTapped(_ sender: UIButton) {
+    }
+    
+    //MARK : - 스토리보드에서의 화면이동(간접 세그웨이)
+    @IBAction func storyboardWithSegueButtonTapped(_ sender: UIButton) {
+    }
+    
+    
 }
 
