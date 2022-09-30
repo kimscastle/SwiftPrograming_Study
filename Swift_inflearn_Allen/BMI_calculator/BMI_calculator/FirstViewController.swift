ffafdec27d20b984e8bb0e4a4334d4fabb8b48b7
kimscastle <uisong26559287@gmail.com>
2022-09-30 23:22:27 +0900

    :lipstick: 코드로 첫번째view ui구현

--- Swift_inflearn_Allen/BMI_calculator/BMI_calculator/FirstViewController.swift
+++ Swift_inflearn_Allen/BMI_calculator/BMI_calculator/FirstViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 import SnapKit
 
 class FirstViewController: UIViewController {
+    
+    let mainLabel = UILabel()
+    let backButton = UIButton(type: .custom)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        render()
 
-        // Do any additional setup after loading the view.
+    }
+    
+    func render() {
+        mainLabel.text = "FirstViewController"
+        view.addSubview(mainLabel)
+        mainLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        backButton.setTitle("Back", for: .normal)
+        backButton.setTitleColor(.white, for: .normal)
+        backButton.backgroundColor = .blue
+        backButton.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        view.addSubview(backButton)
+        backButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.height.equalTo(40)
+            make.width.equalTo(70)
+            make.bottom.equalToSuperview().inset(40)
+        }
+    }
+    
+    @objc func backButtonTapped() {
+        
     }
     
 
