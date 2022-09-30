96e859d2dafa211d6d74ee985d7aca620c306585
kimscastle <uisong26559287@gmail.com>
2022-09-30 23:35:30 +0900

    :sparkles: 화면전달과 데이터전달 기능 구현

--- Swift_inflearn_Allen/BMI_calculator/BMI_calculator/FirstViewController.swift
+++ Swift_inflearn_Allen/BMI_calculator/BMI_calculator/FirstViewController.swift
@@ -12,15 +12,21 @@ class FirstViewController: UIViewController {
     
     let mainLabel = UILabel()
     let backButton = UIButton(type: .custom)
+    
+    //MARK : - 이전 view에서 넘어온 string을 받을 변수(옵셔널로 선언하는경우가 많음)
+    // 아니면 생성자로 초기화를 해줘야한다
+    var someString: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .gray
         render()
 
     }
     
     func render() {
-        mainLabel.text = "FirstViewController"
+        //MARK : - ".text"가 이미 옵셔널 타입이기때문에 someString을 언래핑하지 않아도 괜찮음
+        mainLabel.text = someString
         view.addSubview(mainLabel)
         mainLabel.snp.makeConstraints { make in
             make.center.equalToSuperview()
@@ -40,8 +46,9 @@ class FirstViewController: UIViewController {
         }
     }
     
+    //MARK : - 뒤로가기 버튼
     @objc func backButtonTapped() {
-        
+        dismiss(animated: true)
     }
     
 
--- Swift_inflearn_Allen/BMI_calculator/BMI_calculator/ViewController.swift
+++ Swift_inflearn_Allen/BMI_calculator/BMI_calculator/ViewController.swift
@@ -16,6 +16,22 @@ class ViewController: UIViewController {
 
     //MARK : - 코드로 화면이동(다음화면이 코드로 작성되어있을때 사용)
     @IBAction func codeNextButtonTapped(_ sender: UIButton) {
+        // 1.넘어갈 화면을 선언해준다
+        let firstVC = FirstViewController()
+        
+        // 보통 다음뷰에서 데이터를 받기위한 변수에다가 데이터를 넘겨준다
+        firstVC.someString = "아기상어"
+        
+        // 아래같은 방식으로는 잘 사용하지 않는다(직접적으로 레이블에 접근하는 방식은 잘 사용하지 않는다)
+        // 다음뷰가 스토리보드라면 이런 방식은 불가능한 접근법이다 
+        //firstVC.mainLabel.text = "아기상어"
+        
+        // 1-1.어떻게 넘어갈지를 선택해줄 수 있다
+        firstVC.modalPresentationStyle = .fullScreen
+        
+        // 2.이동하는 코드는 uiviewcontroller에 이미 구현이 되어있다 -> present
+        // completion은 보통 nil이기때문에 생략도 가능
+        present(firstVC, animated: true)
     }
     
     //MARK : - 코드로 스토리보드 객체를 생성해서 화면이동
