0907215efa0e3ce09afcc233968d61d8c0bbc472
kimscastle <uisong26559287@gmail.com>
2022-09-30 22:43:20 +0900

    :tada: BMI계산기 프로젝트 생성

---
+++ Swift_inflearn_Allen/BMI_calculator/BMI_calculator/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BMI_calculator
+//
+//  Created by uiskim on 2022/09/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+}
+
