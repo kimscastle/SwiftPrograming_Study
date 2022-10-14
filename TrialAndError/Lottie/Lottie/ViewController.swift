88e2d3d091937c62fd9af1e1176672941ea7c9df
kimscastle <uisong26559287@gmail.com>
2022-10-14 18:44:25 +0900

    :tada: Lottie적용해보기 프로젝트 생성

---
+++ TrialAndError/Lottie/Lottie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lottie
+//
+//  Created by uiskim on 2022/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
