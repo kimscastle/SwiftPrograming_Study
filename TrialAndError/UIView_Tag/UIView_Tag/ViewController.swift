1050b52037ca4297ebb1665d4477800d89fba93d
kimscastle <uisong26559287@gmail.com>
2022-10-25 22:04:40 +0900

    :tada: UIViewTag프로젝트 생성

---
+++ TrialAndError/UIView_Tag/UIView_Tag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIView_Tag
+//
+//  Created by uiskim on 2022/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
