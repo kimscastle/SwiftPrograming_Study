1fd44cd8c9b54445cc8b7c2080da195ac0972145
kimscastle <uisong26559287@gmail.com>
2023-02-11 12:33:48 +0900

    :tada: cell 재사용문제 protocol과 extension으로 해결하기 프로젝트 생성

---
+++ TrialAndError/CellReuseProblem/CellReuseProblem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CellReuseProblem
+//
+//  Created by uiskim on 2023/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
