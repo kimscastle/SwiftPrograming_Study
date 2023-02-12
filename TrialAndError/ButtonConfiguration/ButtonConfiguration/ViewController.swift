48a74c6eab2e1d22cd04eba7f905789a0ec3572f
kimscastle <uisong26559287@gmail.com>
2023-02-12 19:29:36 +0900

    :tada: ios 15 button congfiguration 프로젝트 생성

---
+++ TrialAndError/ButtonConfiguration/ButtonConfiguration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ButtonConfiguration
+//
+//  Created by uiskim on 2023/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
