af8152132189fbbc9fc2db2dec4743defca050e0
kimscastle <uisong26559287@gmail.com>
2023-02-23 17:18:25 +0900

    :tada: BaseViewController 프로젝트 생성 및 패키지(snapkit, then)추가

---
+++ TrialAndError/BaseViewController/BaseViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseViewController
+//
+//  Created by uiskim on 2023/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
