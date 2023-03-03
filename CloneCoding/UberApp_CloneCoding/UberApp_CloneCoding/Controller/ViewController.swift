8cc6be6d864552daf917082ffc99aaa6cdd3ec24
kimscastle <uisong26559287@gmail.com>
2023-03-03 19:49:50 +0900

    :building_construction: 폴더구조 만들기

--- CloneCoding/UberApp_CloneCoding/UberApp_CloneCoding/Controller/ViewController.swift
+++ CloneCoding/UberApp_CloneCoding/UberApp_CloneCoding/Controller/ViewController.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import SnapKit
+import Then
+
+class LoginController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
