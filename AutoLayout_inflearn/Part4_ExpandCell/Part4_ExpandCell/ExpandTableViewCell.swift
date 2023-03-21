0694a7dc203de2b2fe01bb905ad7c2bb044f1e52
kimscastle <uisong26559287@gmail.com>
2023-03-21 23:01:20 +0900

    :sparkles: Cell expand기능 구현

---
+++ AutoLayout_inflearn/Part4_ExpandCell/Part4_ExpandCell/ExpandTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ExpandTableViewCell.swift
+//  Part4_ExpandCell
+//
+//  Created by uiskim on 2023/03/21.
+//
+
+import UIKit
+
+class ExpandTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
