251f6419325b774962b31af8dd5288bd73b606a9
kimscastle <uisong26559287@gmail.com>
2023-03-24 13:51:39 +0900

    :lipstick: custom switch 구현

--- AutoLayout_inflearn/Part8_CustomSwitch/Part8_CustomSwitch/ViewController.swift
+++ AutoLayout_inflearn/Part8_CustomSwitch/Part8_CustomSwitch/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var switchBtn: UIButton!
+    @IBOutlet weak var switchView: UIView!
+    @IBOutlet weak var switchCenterX: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        switchBtn.layer.cornerRadius = switchBtn.bounds.height/2
+        switchView.layer.cornerRadius = switchView.bounds.height/2
     }
 
-
+    @IBAction func selectedButton(_ sender: UIButton) {
+        let leftMaxWidth = CGFloat.getDistance(firstItem: switchView.bounds.width, SecondItem: switchBtn.bounds.width)
+        
+        switchCenterX.constant = switchCenterX.constant == leftMaxWidth ? -leftMaxWidth : leftMaxWidth
+        switchView.backgroundColor = switchCenterX.constant == leftMaxWidth ? UIColor.yellow : UIColor.blue
+        
+        UIView.animate(withDuration: 0.2) {
+            self.view.layoutIfNeeded()
+        }
+    }
 }
 
+extension CGFloat {
+    static func getDistance(firstItem: CGFloat, SecondItem: CGFloat) -> CGFloat {
+        return (firstItem/2)-(SecondItem/2)
+    }
+}
