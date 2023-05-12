a4c080a0daa361f2ae26d0f5df162ecfb6a92558
kimscastle <99013115+kimscastle@users.noreply.github.com>
2023-05-12 15:16:35 +0900

    :sparkles: Generic type 변경

--- TrialAndError/DI_Practice/APIKit/APICaller.swift
+++ TrialAndError/DI_Practice/APIKit/APICaller.swift
@@ -12,7 +12,7 @@ public class APICaller {
     public static let shared = APICaller()
     private init() {}
     
-    public func fetchCoures<T: Codable>(returnType: [T].Type, completion: @escaping([T]) -> Void) {
+    public func fetchCoures<T: Codable>(returnType: T.Type, completion: @escaping(T) -> Void) {
         guard let url = URL(string: "https://iosacademy.io/api/v1/courses/index.php") else { return }
         let task = URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data, error == nil else { return }
