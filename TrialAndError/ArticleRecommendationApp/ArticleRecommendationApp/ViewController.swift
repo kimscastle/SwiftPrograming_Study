7f2d256c6fbf38b68461fe32b4a4ec17974816a7
kimscastle <uiskim22@pos.idserve.net>
2023-06-13 18:47:34 +0900

    :tada: CoreML 추천시스템 프로젝트 생성 및 초기세팅

---
+++ TrialAndError/ArticleRecommendationApp/ArticleRecommendationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArticleRecommendationApp
+//
+//  Created by uiskim on 2023/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
