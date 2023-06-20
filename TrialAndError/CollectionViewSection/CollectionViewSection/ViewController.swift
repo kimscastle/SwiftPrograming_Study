99ee2c4e6af89db5719ce8b8c7f83113a2ec630b
kimscastle <uiskim22@pos.idserve.net>
2023-06-20 11:29:47 +0900

    :tada: Collectionview section 프로젝트 생성

---
+++ TrialAndError/CollectionViewSection/CollectionViewSection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewSection
+//
+//  Created by uiskim on 2023/06/20.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
