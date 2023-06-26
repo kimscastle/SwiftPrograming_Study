f612244f428ad5271f683cd1961e6bb8217697e4
kimscastle <uiskim22@pos.idserve.net>
2023-06-26 21:02:59 +0900

    :tada: 라이온하트 아티클뷰 프로젝트 생성 및 세팅

---
+++ TrialAndError/LionHeart_ArticleView/LionHeart_ArticleView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LionHeart_ArticleView
+//
+//  Created by uiskim on 2023/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
