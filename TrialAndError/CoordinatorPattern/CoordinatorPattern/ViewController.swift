98c0d885949be778ea6f727f8a7264f561491872
kimscastle <uiskim22@pos.idserve.net>
2023-09-26 16:04:20 +0900

    :tada: 코디네이터패턴 연습

---
+++ TrialAndError/CoordinatorPattern/CoordinatorPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by uiskim on 2023/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
