44292006be2fc08f7f00636b60e3e1334b9c724f
kimscastle <uiskim22@pos.idserve.net>
2023-10-03 10:54:21 +0900

    ✨ 코디네이터 시작점 적용

--- TrialAndError/CoordinatorPattern/CoordinatorPattern/SceneDelegate.swift
+++ TrialAndError/CoordinatorPattern/CoordinatorPattern/SceneDelegate.swift
@@ -11,12 +11,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: LoginViewController())
+//        let navigationController = UINavigationController(rootViewController: LoginViewController())
+        let navigationController = UINavigationController()
+        appCoordinator = AppCoordinator(navigationController: navigationController)
+        appCoordinator!.start()
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
     }
